8cc481950c62ba7a7d3a40f246d7c2aa586aafe9
agudelotmateo <agudelotmateo@gmail.com>
2018-04-21 11:38:33 -0500

    Added ignored face list feature

--- lambda_function.py
+++ lambda_function.py
@@ -23,7 +23,7 @@ def dummy():
         if len(inputFilename):
             if deb:
                 print('File being processed: {}'.format(inputFilename))
-                print('*Identifying faces...')
+                print('  Identifying faces...')
             response = client.detect_faces(
                 Image={
                     'S3Object': {
@@ -35,54 +35,82 @@ def dummy():
             )
 
             if deb:
-                print('*Gathering original file...')
+                print('  Gathering original file...')
             inputImage = getImage(inputPath, inputBucket)
             (inputImageWidth, inputImageHeigth) = inputImage.size
 
+            ignored = set()
+            for ignoredInput in s3Resource.Bucket(inputBucket).objects.filter(Prefix='Ignored/'):
+                if ignoredInput.key[-1] != '/':
+                    comparison = client.compare_faces(
+                        SimilarityThreshold=70,
+                        SourceImage={
+                            'S3Object': {
+                                'Bucket': inputBucket,
+                                'Name': ignoredInput.key
+                            }
+                        },
+                        TargetImage={
+                            'S3Object': {
+                                'Bucket': inputBucket,
+                                'Name': inputPath
+                            }
+                        }
+                    )
+                    for faceMatch in comparison['FaceMatches']:
+                        ignored.add(json.dumps(faceMatch['Face']['BoundingBox']))
+
             if deb:
-                print('*Processing each face...')
+                print('  Processing each face...')
             faceCounter = 1
             for faceDetail in response['FaceDetails']:
                 if deb:
-                    print('**Processing face {}/{}'.format(faceCounter,
+                    print('    Processing face {}/{}'.format(faceCounter,
                                                            len(response['FaceDetails'])))
-                    print('**Identifying predominant emotion...')
-                detectedEmotion = ''
-                maxValue = -1.0
-                for emotion in faceDetail['Emotions']:
-                    confidence = float(emotion['Confidence'])
-                    if confidence > maxValue:
-                        maxValue = confidence
-                        detectedEmotion = emotion['Type'].encode(
-                            'ascii', 'ignore').lower()
+                if json.dumps(faceDetail['BoundingBox']) not in ignored:
+                    if deb:
+                        print('    Identifying predominant emotion...')
+                    detectedEmotion = ''
+                    maxValue = -1.0
+                    for emotion in faceDetail['Emotions']:
+                        confidence = float(emotion['Confidence'])
+                        if confidence > maxValue:
+                            maxValue = confidence
+                            detectedEmotion = emotion['Type'].encode(
+                                'ascii', 'ignore').lower()
 
-                if deb:
-                    print('**Identifying face location...')
-                faceUpperLeftX = int(
-                    faceDetail['BoundingBox']['Left']*inputImageWidth)
-                faceUpperLeftY = int(
-                    faceDetail['BoundingBox']['Top']*inputImageHeigth)
-                faceHeight = int(
-                    faceDetail['BoundingBox']['Height']*inputImageHeigth)
-                faceWidth = int(
-                    faceDetail['BoundingBox']['Width']*inputImageWidth)
+                    if deb:
+                        print('    Identifying face location...')
+                    faceUpperLeftX = int(
+                        faceDetail['BoundingBox']['Left']*inputImageWidth)
+                    faceUpperLeftY = int(
+                        faceDetail['BoundingBox']['Top']*inputImageHeigth)
+                    faceHeight = int(
+                        faceDetail['BoundingBox']['Height']*inputImageHeigth)
+                    faceWidth = int(
+                        faceDetail['BoundingBox']['Width']*inputImageWidth)
 
-                if deb:
-                    print('**Gathering emoji...')
-                emoji = getImage('Emojis/'+detectedEmotion+'.png', inputBucket)
+                    if deb:
+                        print('    Gathering emoji...')
+                    emoji = getImage(
+                        'Emojis/'+detectedEmotion+'.png', inputBucket)
 
-                if deb:
-                    print('**Resizing emoji...')
-                emoji = emoji.resize((faceWidth, faceHeight), Image.ANTIALIAS)
+                    if deb:
+                        print('    Resizing emoji...')
+                    emoji = emoji.resize(
+                        (faceWidth, faceHeight), Image.ANTIALIAS)
 
-                if deb:
-                    print('**Pasting emoji...')
-                inputImage.paste(
-                    emoji, (faceUpperLeftX, faceUpperLeftY), emoji)
+                    if deb:
+                        print('    Pasting emoji...')
+                    inputImage.paste(
+                        emoji, (faceUpperLeftX, faceUpperLeftY), emoji)
+                else:
+                    if deb:
+                        print('    Ignored!')
                 faceCounter += 1
 
             if deb:
-                print('*Uploading new file...')
+                print('  Uploading new file...')
             upload_image(inputImage, inputFilename, outputBucket)
 
             if deb:
