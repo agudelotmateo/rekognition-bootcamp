3b719993827f0e1bb2839a62f1bafee52b699422
agudelotmateo <agudelotmateo@gmail.com>
2018-04-21 08:38:07 -0500

    Minor indentation changes

--- lambda_function.py
+++ lambda_function.py
@@ -7,12 +7,14 @@ import os
 inputBucket = 'eafit-team5-input'
 outputBucket = 'eafit-team5-output'
 s3Resource = boto3.resource('s3')
-client = boto3.client('rekognition','us-east-1')
+client = boto3.client('rekognition', 'us-east-1')
 
 deb = False
 
+
 def lambda_handler(event, context):
     dummy()
+
 
 def dummy():
     for inputImages in s3Resource.Bucket(inputBucket).objects.filter(Prefix='People/'):
@@ -24,60 +26,68 @@ def dummy():
                 print('*Identifying faces...')
             response = client.detect_faces(
                 Image={
-                    'S3Object':{
-                        'Bucket':inputBucket,
-                        'Name':inputPath
+                    'S3Object': {
+                        'Bucket': inputBucket,
+                        'Name': inputPath
                     }
                 },
                 Attributes=['ALL']
             )
-            
+
             if deb:
                 print('*Gathering original file...')
             inputImage = getImage(inputPath, inputBucket)
             (inputImageWidth, inputImageHeigth) = inputImage.size
-            
+
             if deb:
                 print('*Processing each face...')
             faceCounter = 1
             for faceDetail in response['FaceDetails']:
                 if deb:
-                    print('**Processing face {}/{}'.format(faceCounter, len(response['FaceDetails'])))
+                    print('**Processing face {}/{}'.format(faceCounter,
+                                                           len(response['FaceDetails'])))
                     print('**Identifying predominant emotion...')
-                detectedEmotion = ''        
+                detectedEmotion = ''
                 maxValue = -1.0
                 for emotion in faceDetail['Emotions']:
                     confidence = float(emotion['Confidence'])
                     if confidence > maxValue:
                         maxValue = confidence
-                        detectedEmotion = emotion['Type'].encode('ascii', 'ignore').lower()
-                        
+                        detectedEmotion = emotion['Type'].encode(
+                            'ascii', 'ignore').lower()
+
                 if deb:
                     print('**Identifying face location...')
-                faceUpperLeftX = int(faceDetail['BoundingBox']['Left']*inputImageWidth)
-                faceUpperLeftY = int(faceDetail['BoundingBox']['Top']*inputImageHeigth)
-                faceHeight = int(faceDetail['BoundingBox']['Height']*inputImageHeigth)
-                faceWidth = int(faceDetail['BoundingBox']['Width']*inputImageWidth)
-                
+                faceUpperLeftX = int(
+                    faceDetail['BoundingBox']['Left']*inputImageWidth)
+                faceUpperLeftY = int(
+                    faceDetail['BoundingBox']['Top']*inputImageHeigth)
+                faceHeight = int(
+                    faceDetail['BoundingBox']['Height']*inputImageHeigth)
+                faceWidth = int(
+                    faceDetail['BoundingBox']['Width']*inputImageWidth)
+
                 if deb:
                     print('**Gathering emoji...')
                 emoji = getImage('Emojis/'+detectedEmotion+'.png', inputBucket)
-                
+
                 if deb:
                     print('**Resizing emoji...')
                 emoji = emoji.resize((faceWidth, faceHeight), Image.ANTIALIAS)
-                
+
                 if deb:
                     print('**Pasting emoji...')
-                inputImage.paste(emoji, (faceUpperLeftX, faceUpperLeftY), emoji)
+                inputImage.paste(
+                    emoji, (faceUpperLeftX, faceUpperLeftY), emoji)
                 faceCounter += 1
-                
+
             if deb:
                 print('*Uploading new file...')
             upload_image(inputImage, inputFilename, outputBucket)
-            
+
             if deb:
                 print('Done with file {}\n'.format(inputFilename))
+
 
 def getImage(path, bucket):
     locaPath = '/tmp/' + path.split('/')[-1]
@@ -85,6 +95,7 @@ def getImage(path, bucket):
     image = Image.open(locaPath)
     os.remove(locaPath)
     return image
+
 
 def upload_image(image, filename, bucket):
     tmpPath = '/tmp/' + filename
